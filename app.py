dac69a8b80348325ca80c99d9da498202d3dbbdb
jzy2003 <2646696633@qq.com>
2026-05-11 22:18:31 +0800

    更新双人报名系统

--- app.py
+++ app.py
@@ -13,9 +13,26 @@ db = SQLAlchemy(app)
 
 # 创建报名数据表
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
+
+    # 本人信息
     name = db.Column(db.String(100))
     phone = db.Column(db.String(100))
+    department = db.Column(db.String(100))
+    grade = db.Column(db.String(100))
+    card_number = db.Column(db.String(100))
+
+    # 搭档信息
+    partner_name = db.Column(db.String(100))
+    partner_phone = db.Column(db.String(100))
+    partner_department = db.Column(db.String(100))
+    partner_grade = db.Column(db.String(100))
+    partner_card_number = db.Column(db.String(100))
+
+    # 晚宴
+    dinner = db.Column(db.String(20))
+
 
 # 首页（报名页面）
 @app.route('/', methods=['GET', 'POST'])
@@ -23,16 +40,39 @@ def home():
 
     if request.method == 'POST':
 
-        # 获取表单数据
+        # 本人信息
         name = request.form.get('name')
         phone = request.form.get('phone')
+        department = request.form.get('department')
+        grade = request.form.get('grade')
+        card_number = request.form.get('card_number')
 
-        # 调试输出（会显示在终端）
-        print("姓名：", name)
-        print("电话：", phone)
+        # 搭档信息
+        partner_name = request.form.get('partner_name')
+        partner_phone = request.form.get('partner_phone')
+        partner_department = request.form.get('partner_department')
+        partner_grade = request.form.get('partner_grade')
+        partner_card_number = request.form.get('partner_card_number')
 
-        # 保存到数据库
-        new_user = User(name=name, phone=phone)
+        # 晚宴
+        dinner = request.form.get('dinner')
+
+        # 保存数据库
+        new_user = User(
+            name=name,
+            phone=phone,
+            department=department,
+            grade=grade,
+            card_number=card_number,
+
+            partner_name=partner_name,
+            partner_phone=partner_phone,
+            partner_department=partner_department,
+            partner_grade=partner_grade,
+            partner_card_number=partner_card_number,
+
+            dinner=dinner
+        )
 
         db.session.add(new_user)
         db.session.commit()
@@ -40,6 +80,7 @@ def home():
         return "报名成功！"
 
     return render_template('index.html')
+
 
 # 后台页面
 @app.route('/admin')
@@ -49,6 +90,7 @@ def admin():
     users = User.query.all()
 
     return render_template('admin.html', users=users)
+
 
 # 启动程序
 if __name__ == '__main__':
